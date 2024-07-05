6e9b6749a5e90b943323cb1fe481581429ee00bc
jimin Lee <dlwlasl1207@gmail.com>
2024-07-05 18:20:52 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240705/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 240705/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -1,13 +1,15 @@
 element_cnt = int(input())
-elements = list(map(int, input().split()))
+# elements = list(map(int, input().split()))
+elements = [3,2,3,1]
 
 elements.sort()
 
 already_exist = [x for i, x in enumerate(elements) if i != elements.index(x)]
+# print(already_exist)
 
-for element in elements:
-    if element in already_exist:
-        elements.remove(element)
+elements = [x for x in elements if x not in already_exist]
+
+# print(*elements)
 
 if not elements:
     print(-1)
