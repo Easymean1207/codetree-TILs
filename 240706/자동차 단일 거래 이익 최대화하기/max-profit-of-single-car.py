59d8e2319a3e8a017c94e86f714c16404b20ac0e
jimin Lee <dlwlasl1207@gmail.com>
2024-07-06 21:21:11 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 240706/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 240706/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -7,11 +7,13 @@ price_of_each_year = list(map(int, input().split()))
 min_price = sys.maxsize
 max_profit = 0
 
+# 최소 가격 갱신의 경우 : 최소 가격을 갱신
+# 최소 가격 갱신이 아닌 경우: (현재 가격 - 최소 가격)과 최대 이익 비교
 for price in price_of_each_year:
     # 현재 가격이 최소 가격보다 작으면 최소 가격 갱신
     if price < min_price:
         min_price = price
-        
+
     # 현재 가격과 최소 가격의 차이를 계산하여 최대 이익 갱신
     elif price - min_price > max_profit:
         max_profit = price - min_price
