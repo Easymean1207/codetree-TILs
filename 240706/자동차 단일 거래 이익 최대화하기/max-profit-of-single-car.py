b217d4a34ae3ea4e67768a3ac28edcd5e2a4fbb9
jimin Lee <dlwlasl1207@gmail.com>
2024-07-06 20:53:39 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240706/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 240706/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -7,9 +7,8 @@ min_idx = price_of_each_year.index(min_price)
 
 # 최소 인덱스 ~ 끝 중 가장 큰 값을 확인
 max_price = max(price_of_each_year[min_idx:])
-max_idx = price_of_each_year.index(max_price)
 
-# 이익: -(min_price - max_price)
+# 이익: (min_price - max_price)
 profit = -(min_price - max_price)
 
 if profit > 0:
