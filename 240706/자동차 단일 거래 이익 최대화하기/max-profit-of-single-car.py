db03460d45049aa61781d497609835033360a0ce
jimin Lee <dlwlasl1207@gmail.com>
2024-07-06 21:11:20 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240706/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 240706/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -3,14 +3,22 @@ import sys
 n_year = int(input())
 price_of_each_year = list(map(int, input().split()))
 
-min_price = sys.maxsize
-profit = 0
+min_price = price_of_each_year[0]
+max_profit = 0
 
-for price in price_of_each_year:
+# 핵심 아이디어: 사는 시기가 아닌 파는 시기를 기준으로 생각
+# 파는 시기에서 가장 최소가 되는 인덱스의 값을 빼면 그것이 최대 이익
+for price,i in enumerate(price_of_each_year):
+    # 현재 가격에서 이익 계산
+    profit = price[i] - min_price
 
-    if price < min_price:
-        min_price = price
-    elif price - min_price > profit:
-        profit = price - min_price
+    if profit > max_profit:
+        max_profit = profit
+    
+    if min_price > price[i]:
+        min_price[i]
 
-print(profit)
+print(max_profit)    
+    
+
+print(max_profit)
