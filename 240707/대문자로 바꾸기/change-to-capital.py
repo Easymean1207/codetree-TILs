8bd402331938dfebf7b231929a0c3ead2b58cc6e
jimin Lee <dlwlasl1207@gmail.com>
2024-07-07 17:47:01 +0900

    Title: 대문자로 바꾸기, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240707/대문자로 바꾸기/change-to-capital.py
+++ 240707/대문자로 바꾸기/change-to-capital.py
@@ -1,9 +1,12 @@
 n = 5
+
+# list comprehension을 통한 리스트 선언
 arr_2d =[
     list(input().split())
     for _ in range(n)
 ]
 
+# 2중 for문 사용
 for i in range(n):
     for j in range(len(arr_2d[i])):
         print(arr_2d[i][j].upper(), end = " ")
