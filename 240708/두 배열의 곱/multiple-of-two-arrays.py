5235725f62e24e121a1e62f44e955d6b51ec05c6
jimin Lee <dlwlasl1207@gmail.com>
2024-07-08 16:19:03 +0900

    Title: 두 배열의 곱, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240708/두 배열의 곱/multiple-of-two-arrays.py
+++ 240708/두 배열의 곱/multiple-of-two-arrays.py
@@ -12,17 +12,19 @@ arr_2nd = [
     for _ in range(3)
 ]
 
+
 ''' for문을 이용한 방식 '''
 # arr_mul(element-wise 방식의 곱이 들어간 배열)
-# arr_mul = [
-#     [0 for _ in range(len(arr_1st[0]))]
-#     for _ in range(3)
-# ]
+arr_mul = [
+    [0 for _ in range(len(arr_1st[0]))]
+    for _ in range(3)
+]
 
-# for i in range(3):
-#     for j in range(len(arr_1st[0])):
-#         arr_mul[i][j] = arr_1st[i][j] * arr_2nd[i][j]
-    
+for i in range(3):
+    for j in range(len(arr_1st[0])):
+        arr_mul[i][j] = arr_1st[i][j] * arr_2nd[i][j]
+
+
 ''' 리스트 컴프리헨션을 이용한 방식 '''
 arr_mul = [
     [arr_1st[i][j] * arr_2nd[i][j] for j in range(len(arr_1st[0]))]
