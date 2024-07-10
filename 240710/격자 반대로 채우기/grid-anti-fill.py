cad48acb0211a8ca97135e3acc19783f03d6d03a
jimin Lee <dlwlasl1207@gmail.com>
2024-07-10 16:29:30 +0900

    Title: 격자 반대로 채우기, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240710/격자 반대로 채우기/grid-anti-fill.py
+++ 240710/격자 반대로 채우기/grid-anti-fill.py
@@ -10,17 +10,34 @@ mat = [
 value = 1
 
 for col in range(n-1, -1, -1):
-    # col 값이 짝수인 경우 -> 정방향
-    if col % 2 == 0:
-        for row in range(n):
-            mat[row][col] = value
-            value += 1
+    # n이 짝수일 때
+    if n % 2 == 0:
+        # col 값이 짝수인 경우 -> 정방향
+        if col % 2 == 0:
+            for row in range(n):
+                mat[row][col] = value
+                value += 1
 
-    # col 값이 홀수인 경우 -> 역방향
+        # col 값이 홀수인 경우 -> 역방향
+        else:
+            for row in range(n - 1, -1, -1):
+                mat[row][col] = value
+                value += 1
+    
+    # n이 홀수 일때
     else:
-        for row in range(n - 1, -1, -1):
-            mat[row][col] = value
-            value += 1
+        # col 값이 홀수인 경우 -> 정방향
+        if col % 2 != 0:
+            for row in range(n):
+                mat[row][col] = value
+                value += 1
+
+        # col 값이 짝수인 경우 -> 역방향
+        else:
+            for row in range(n - 1, -1, -1):
+                mat[row][col] = value
+                value += 1
+
 
 for row in range(n):
     for column in range(n):
