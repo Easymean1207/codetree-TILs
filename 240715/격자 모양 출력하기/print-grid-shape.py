76ddb5a48fe820a7ebdb123a9246b2ce0f05a926
jimin Lee <dlwlasl1207@gmail.com>
2024-07-15 15:59:52 +0900

    Title: 격자 모양 출력하기, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240715/격자 모양 출력하기/print-grid-shape.py
@@ -0,0 +1,16 @@
+# n = 격자의 크기, m = 점의 개수
+n, m = map(int, input().split())
+
+# 기본 격자(nxn) 설정
+placed= [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+# 각 행,열에 값 할당
+for _ in range(m):
+    r,c = map(int, input().split())
+    placed[r-1][c-1] = r * c
+
+for row in placed:
+    print(*row)
