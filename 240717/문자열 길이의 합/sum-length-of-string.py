c79a228b98fdc93a8a9e3646c7f348fb5817dfb0
jimin Lee <dlwlasl1207@gmail.com>
2024-07-17 16:43:18 +0900

    Title: 문자열 길이의 합, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240717/문자열 길이의 합/sum-length-of-string.py
+++ 240717/문자열 길이의 합/sum-length-of-string.py
@@ -6,14 +6,14 @@ words = [
     for _ in range(n)
 ]
 
-# 문자열 길이의 총합, a 등장 횟수 카운트 변수 선언
+# 문자열 길이의 총합, 카운트 변수 선언
 total_length = 0
 cnt = 0
 
 for word in words:
     total_length += len(word)
 
-    if 'a' in word:
-        cnt += word.count('a')
+    if word[0] == 'a':
+        cnt+=1
 
 print(total_length, cnt)
