8698abe94d8cb6bacb2ab15cadce0b95632d0dc7
jimin Lee <dlwlasl1207@gmail.com>
2024-07-19 14:24:19 +0900

    Title: 문자열 나누기, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240719/문자열 나누기/divide-string.py
+++ 240719/문자열 나누기/divide-string.py
@@ -3,10 +3,12 @@ n = input()
 # 리스트 형태로 입력받음
 num_ls = list(input().split())
 
+# 리스트를 공백 제거한 문자열로 변환
 num_str = ''.join(num for num in num_ls)
 
 new_line_added = ''
 
+# 5번째 element마다 줄 바꿈
 for i in range(len(num_str)):
     new_line_added += num_str[i]
     if i % 5 == 4:
