8b123ade95bd3b7bfd846c43af3ca101401f0d6f
jimin Lee <dlwlasl1207@gmail.com>
2024-07-20 21:53:20 +0900

    Title: 특정 문자의 등장 횟수, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240720/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
+++ 240720/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -3,5 +3,10 @@ word = input()
 key1 = 'ee'
 key2 = 'eb'
 
-print(word.count(key1), end = ' ')
-print(word.count(key2), end = ' ')
+key1_cnt = 0
+
+for i in range(len(word)-1):
+    if word[i] == 'e' and word[i+1] == 'e':
+        key1_cnt+=1
+
+print(key1_cnt, word.count(key2))
