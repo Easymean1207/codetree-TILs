4c635d5b4f1da440671777e88fe0f817cae1b1f8
jimin Lee <dlwlasl1207@gmail.com>
2024-07-23 16:43:46 +0900

    Title: 문자 제거하기, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 240723/문자 제거하기/del-char.py
+++ 240723/문자 제거하기/del-char.py
@@ -1,9 +1,9 @@
 word = input()
 arr = list(word)
 
-for _ in range(len(arr)-1):
+while len(arr) > 1:
     idx = int(input())
-    if idx > len(arr):
+    if idx >= len(arr):
         idx = -1
     arr.pop(idx)
     print(''.join(arr))
