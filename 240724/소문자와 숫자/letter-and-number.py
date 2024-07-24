9bc200e532a25b298863b0133a05603e9df2db8e
jimin Lee <dlwlasl1207@gmail.com>
2024-07-24 18:36:10 +0900

    Title: 소문자와 숫자, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240724/소문자와 숫자/letter-and-number.py
@@ -0,0 +1,8 @@
+string = input()
+
+for char in string:
+    if char.isalnum():
+        if char.isdigit():
+            print(char, end = '')
+        elif char.isalpha():
+            print(char.lower(), end = '')
