f4d23e73790523883ef450376b571c15c252e3b6
jimin Lee <dlwlasl1207@gmail.com>
2024-07-26 00:49:54 +0900

    Title: 짝수이면서 합이 5의 배수인 수, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240726/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.py
@@ -0,0 +1,15 @@
+# n: 10 ~ 99
+def is_magic_number(n):
+    digit_sum = sum([int(i) for i in str(n)])
+    
+    return n % 2 == 0 and digit_sum % 5 == 0
+
+def print_yes_no(condition):
+    if condition:
+        print('Yes')
+    else:
+        print('No')
+
+n = int(input())
+
+print_yes_no(is_magic_number(n))
