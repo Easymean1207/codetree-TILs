d2188df49c5844a82a49fcea5ab223242bf494f0
jimin Lee <dlwlasl1207@gmail.com>
2024-07-26 01:55:26 +0900

    Title: 함수를 이용한 소수 판별, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240726/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,30 @@
+import math
+
+# 에라토스테네스의 체
+def eratosthenes(n):
+    # 모든 수를 일단 True로 가정
+    sieve = [True for _ in range(n+1)]
+    # 0, 1 제외
+    sieve[0] = False
+    sieve[1] = False
+
+    for i in range(2, int(math.sqrt(n))+1):
+        # i가 소수인 경우
+        if sieve[i] == True:
+            j = 2
+            # i의 배수들을 모두 지움
+            while i * j <= n:
+                sieve[i * j] = False
+                j+=1
+    
+    return sieve
+
+
+def sumOfPrimes(a,b):
+    sieve = eratosthenes(b)
+
+    return sum(i for i in range(a, b+1) if sieve[i])
+         
+
+a,b = tuple(map(int, input().split()))
+print(sumOfPrimes(a,b))
