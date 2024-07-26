3a08d12eb449b2eadf2b9008970228a18060c6bf
jimin Lee <dlwlasl1207@gmail.com>
2024-07-26 16:35:25 +0900

    Title: 사칙연산 함수, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240726/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,39 @@
+def plus(a, b):
+    return a + b
+
+def minus(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    try:
+        a / b
+    except ZeroDivisionError as e:
+        return e
+
+    return a / b
+
+def chooseOperation(a, b , operator = ''):
+    result = 0
+
+    if operator == '+':
+        result = plus(a,b)
+    elif operator == '-':
+        result = minus(a,b)
+    elif operator == '*':
+        result = multiply(a,b)
+    elif operator == '/':
+        result = divide(a,b)
+    else:
+        return False
+    
+    return result
+
+
+a, o ,c = input().split()
+a, c = int(a), int(c)
+result = chooseOperation(a,c,o)
+
+print(f'{a} {o} {c} = {result}')
