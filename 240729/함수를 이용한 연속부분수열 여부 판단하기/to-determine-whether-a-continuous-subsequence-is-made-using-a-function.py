7618f2f8b038f799180e9bf8af8fc5779e610606
jimin Lee <dlwlasl1207@gmail.com>
2024-07-29 17:46:48 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240729/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 240729/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -1,15 +1,19 @@
 def isContinuousSub(main, target):
-    main, target = str(main), str(target)
+    # 리스트를 문자열로 변환, 구분자를 추가
+    main = ','.join(map(str, main))
+    target = ','.join(map(str, target))
 
+    # find 메서드를 사용하여 target의 시작 인덱스 찾기
     if main.find(target) != -1:
         return True
     else:
         return False
 
+# 사용자 입력 받기
 n1, n2 = tuple(map(int, input().split()))
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-result = isContinuousSub(A,B)
-
-print('Yes' if result == True else 'No')
+# 결과 출력
+result = isContinuousSub(A, B)
+print('Yes' if result else 'No')
