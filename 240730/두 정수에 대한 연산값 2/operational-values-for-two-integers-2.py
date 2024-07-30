cc31e843d820da67fd9e1f5436121dfc7e55072e
jimin Lee <dlwlasl1207@gmail.com>
2024-07-30 17:08:12 +0900

    Title: 두 정수에 대한 연산값 2, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240730/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -0,0 +1,13 @@
+def magicBox(a, b):
+    if a > b:
+        a *=2
+        b +=10
+    else:
+        b *=2
+        a +=10
+
+    return a, b
+
+a, b = tuple(map(int, input().split()))
+a, b = magicBox(a,b)
+print(a,b)
