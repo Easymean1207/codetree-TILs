173ddc8a621a34038b1c4c317ffdaa3d64bd43b8
jimin Lee <dlwlasl1207@gmail.com>
2024-08-02 15:48:09 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240802/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 240802/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -1,7 +1,10 @@
 def recursiveMax(nums):
+    # 원소가 하나라면 그 값이 최대(종료 조건)
     if len(nums) == 1:
         return nums[0]
     
+    # 마지막 원소 (첫번째 ~ 마지막 -1)원소들을 비교하여
+    # 최대값을 찾는 과정을 반복
     max_of_rest = recursiveMax(nums[:-1])
     return max(nums[-1], max_of_rest)
 
