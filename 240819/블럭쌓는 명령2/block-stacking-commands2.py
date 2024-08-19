b842ecfe5db48424f9f20af7aef69df91c72f444
jimin Lee <dlwlasl1207@gmail.com>
2024-08-19 16:36:12 +0900

    Title: 블럭쌓는 명령2, Time: 104ms, Memory: 28MB, Status: Passed - CodeTree

--- 240819/블럭쌓는 명령2/block-stacking-commands2.py
+++ 240819/블럭쌓는 명령2/block-stacking-commands2.py
@@ -1,14 +1,14 @@
-# N개의 칸, K번의 명령
+# N개의 칸, K번의 명령(각 명령당 A~B까지 1개씩 블럭)
 N,K = tuple(map(int, input().split()))
 
-stacked = [0 for i in range (N)]
+stacked = [0 for _ in range (N+1)]
 
-for i in range(K):
+for _ in range(K):
     # 시작점, 끝점 입력 받기
-    start,end = tuple(map(int, input().split()))
+    start, end = tuple(map(int, input().split()))
     
     # 시작점 ~ 끝 채우기
-    for j in range(start, end+1):
-        stacked[j]+=1
+    for i in range(start, end+1):
+        stacked[i]+=1
 
 print(max(stacked))
