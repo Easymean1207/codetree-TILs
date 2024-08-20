ccc7dc48c2d9000f4bf2a33273e3a8f0322fa6a6
jimin Lee <dlwlasl1207@gmail.com>
2024-08-20 18:19:29 +0900

    Title: 왔다 갔던 구역 2, Time: 107ms, Memory: 25MB, Status: Passed - CodeTree

--- 240820/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240820/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -4,6 +4,7 @@
 #     x L -> 왼쪽으로 x만큼 이동
 #     x R -> 오른쪽으로 x만큼 이동 
 # '''
+
 # 최대 범위, 오프셋 설정
 MAX_RANGE = 2000
 OFFSET = 1000
@@ -11,11 +12,11 @@ OFFSET = 1000
 # n번의 명령
 n = int(input())
 
-# 0(1000)에서 시작
-start = OFFSET
+# 0에서 시작
+start = 0
 
 # 최대 이동 ⭐구간⭐ 설정(-1000(0) ~ 1000(2000))
-location = [0 for _ in range(MAX_RANGE+1)]
+location = [0 for _ in range(MAX_RANGE)]
 
 for _ in range(n):
     distance, direction = input().split()
@@ -27,16 +28,16 @@ for _ in range(n):
         start += distance
 
     if direction == 'L':
-        for i in range(start, start-distance, -1):
+        for i in range(start-distance, start):
             location[i] +=1
         start -= distance
 
-cnt = 0
+visit = 0
 for i in range(len(location)):
     if location[i] >=2:
-        cnt += 1
+        visit += 1
 
-print(cnt)
+print(visit)
 
 # segments = []
 # current = 0
