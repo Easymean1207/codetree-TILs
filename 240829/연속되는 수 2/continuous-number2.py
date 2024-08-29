1cbc16b99f29bd8bc7ec6696f598632873157fb6
jimin Lee <dlwlasl1207@gmail.com>
2024-08-29 17:18:47 +0900

    Title: 연속되는 수 2, Time: 168ms, Memory: 25MB, Status: Passed - CodeTree

--- 240829/연속되는 수 2/continuous-number2.py
+++ 240829/연속되는 수 2/continuous-number2.py
@@ -1,7 +1,7 @@
 # 숫자의 개수 N
 N = int(input())
 
-same_cnt = 1
+same_cnt = 0
 result = 0
 nums = []
 
@@ -9,15 +9,14 @@ for i in range(N):
     num = int(input())
     nums.append(num)
 
-# print(nums)
-
 for i in range(N):
     # 연속해서 같은 숫자가 나오는 경우 -> same_cnt를 1 증가
-    if nums[i] == nums[i-1]:
+    if i == 0 or nums[i] == nums[i-1]:
         same_cnt+=1
     # 연속이 끊기는 경우 -> result 재설정, same_cnt를 1로 초기화
     else:
         result = max(result, same_cnt)
         same_cnt = 1
 
+result = max(result, same_cnt)
 print(result)
