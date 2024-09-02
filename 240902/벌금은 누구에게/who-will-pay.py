53bde732c408eef6d2af63cdce4a450b3ff80439
jimin Lee <dlwlasl1207@gmail.com>
2024-09-02 16:03:59 +0900

    Title: 벌금은 누구에게, Time: 132ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240902/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,24 @@
+# 학생 수: N (1 ~ 100)
+# K번(1 ~ 10000) 이상의 벌칙 -> 벌금
+# M번(1 ~ 10000)에 걸쳐 학생의 번호가 주어짐
+
+N,M,K = tuple(map(int, input().split()))
+
+target_list = []
+student = [0] * N
+picked_number = 0
+
+for _ in range(M):
+    # 해당 회차 벌칙 횟수를 누적시킬 학생 번호 선정
+    target = int(input())
+    target_list.append(target)
+
+    # student에 벌칙 정보를 갱신
+    student[target-1]+=1
+
+    # 조건 만족 시 반복문 종료
+    if student[target-1] == K:
+        picked_number = target
+        break
+
+print(picked_number)
